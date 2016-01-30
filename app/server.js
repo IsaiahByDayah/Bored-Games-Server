67b48100cd80ab65a93d2547918b621d6a9fb3b1
Isaiah Smith <jordankid93@gmail.com>
2016-01-30 17:51:41 -0500

    Stringify emits

--- app/server.js
+++ app/server.js
@@ -39,11 +39,11 @@ io.on("connection", function(socket){
 
 	socket.on("JOIN_ROOM", function(clientResponse){
 		socket.join(clientResponse["room"]);
-		io.to(clientResponse["room"]).emit("MESSAGE", clientResponse);
+		io.to(clientResponse["room"]).emit("MESSAGE", JSON.stringify(clientResponse));
 	});
 
 	socket.on("MESSAGE", function(clientResponse){
-		io.to(clientResponse["room"]).emit("MESSAGE", clientResponse);
+		io.to(clientResponse["room"]).emit("MESSAGE", JSON.stringify(clientResponse));
 	});
 
 
